9300a08c6b3ee966a132c30385eb93e6e0659f13
cvrpadmaja0874 <107292493+cvrpadmaja0874@users.noreply.github.com>
2024-08-19 11:25:38 +0530

    Update abc.py

--- Greedy/abc.py
+++ Greedy/abc.py
@@ -1,1 +1,13 @@
+# https://leetcode.com/problems/median-of-two-sorted-arrays/
+def findMedianSortedArrays(nums1, nums2):
+        nums1.extend(nums2)
+        nums1.sort()
+        m=len(nums1)//2
+        if len(nums1)%2!=0:
+            return nums1[m]
+        else:
+            return (nums1[m-1]+nums1[m])/2
 
+nums1 = [1,3]
+nums2 = [2]
+print(findMedianSortedArrays(nums1, nums2))
